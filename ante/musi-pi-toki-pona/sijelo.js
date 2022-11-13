7af17ec786a36eddd21a2b3c516fb5287c6a0b66
Theepicosity <theepicadrian@gmail.com>
2022-11-13 16:45:00 -0700

    ijo sin sin!
    mi pana e lipu sona e lipu sitelen!

--- ante/musi-pi-toki-pona/sijelo.js
+++ ante/musi-pi-toki-pona/sijelo.js
@@ -1,135 +1,70 @@
-document.getElementById("music").src = "music1.wav";
-k = 0 //sina ken ante e ni, taso, musi ni li pini la o ante ala e ni
-kalamamusi = false
-palilon = false
-sitelennimi = ["lonwan"]
-tokinimi = [
-	"IJO PI SUPA JAN",
-	"supa jan: ... <br>supa jan: mi supa tan seme?<br>supa jan: ni li lon mi tan seme??",
-	"supa jan: mi sona ala... <br>supa jan: mi wile tawa la, mi ken kepeken e nasin seme?",
-	"supa jan: a, jan o! <br><jan>jan: seme?",
-	"supa jan: sina ken ala ken pana e sike tawa tawa mi? <br>supa jan: mi wile tawa li wile lukin e ma ale!<br><br>supa jan: awen la... mi wile misikeke e jan.",
-	"<jan>jan: uuu, mi ken alasa e sike tawa... mi sona ala e jo mi pi sike tawa...", //5
-	"<jan>jan: sike tawa li lon ma seme...?",
-	"<jan>jan: oke, mi jo e sike tawa. <br><text>supa jan: a, sina pona!",
-	"supa jan: ni la, mi ken tawa! <br>supa jan: taso... sike tawa ni li... <br>supa jan: ...jo ala e wawa. <br><br>supa jan: mi wile e sike tawa ante.",
-	"<jan>jan: sina wile sike tawa ante la, sina ken alasa e ona kepeken sike tawa. <br><text>supa jan: mi sona, mi sona.",
-	"<jan>jan: mi jo e supa pi ken lawa pi ken toki pi ken tawa. <br>jan: ni li lon mi. <br><text>supa jan: o toki ala.", //10
-	"supa jan: seme li lon tomo ni? <br>supa jan: seme li ken pana e tawa wawa tawa mi?",
-	"supa jan: pona! ni la, mi ken tawa kepeken wawa! <br>supa jan: o tawa tawa mi. mi jo e nasin pona. <br><jan>jan: oke...",
-	"<jan>jan: sina wile kepeken tomo telo, anu seme? <br><text>supa jan: ala, ala, mi lukin taso...",
-	"supa jan: seme? <br><text>supa jan: mi kepeken e tomo telo tan seme? <br><br><jan>jan: mi sona ala... sina ken kepeken e ni tan... <br><br><text>supa jan: sina nasa lili. <br><jan>jan: mi sona.", //ken la jan ale li nasa lili
-	"<jan>jan: a, sina wile tawa tawa ma lon! <br><text>supa jan: lon! o open e lupa. <br><br><jan>jan: ... <br><jan>jan: mi ken ala...", //15
-	"<text>supa jan: tan seme? <br><jan>jan: mi jo ala e luka... <br><text>supa jan: ...",
-	"<text>supa jan: a... ni li ike. <br>supa jan: ken la mi ken alasa e luka.",
-	"",
-	"",
-	"LIPU NANPA WAN LI PINI", //20
+tokinimi_lonwan = [
+	{sitelen:"lonwan0",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(1)">',toki:"IJO PI SUPA JAN"},
+	{sitelen:"lonwan1",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(2)">',toki:"supa jan: ... <br>supa jan: mi supa tan seme?<br>supa jan: ni li lon mi tan seme??"},
+	{sitelen:"lonwan2",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(3)">',toki:"supa jan: mi sona ala... <br>supa jan: mi wile tawa la, mi ken kepeken e nasin seme?"},
+	{sitelen:"lonwan3",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(4)">',toki:"supa jan: a, jan o! <br><jan>jan: seme?"},
+	{sitelen:"lonwan4",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(5)">',toki:"supa jan: sina ken ala ken pana e sike tawa tawa mi? <br>supa jan: mi wile tawa li wile lukin e ma ale!<br><br>supa jan: awen la... mi wile misikeke e jan."},
+	{sitelen:"lonwan5",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(6)">',toki:"<jan>jan: nnn, mi ken alasa e sike tawa... mi sona ala e jo mi pi sike tawa..."}, //5
+	{sitelen:"lonwan6",nasin:'<area shape="rect" coords="222,0,487,60" alt="ilo pi sike tawa" onclick="kama(22)"><area shape="rect" coords="481,87,674,273" alt="lupa lukin" onclick="kama(21)"><area shape="rect" coords="621,353,773,466" alt="kiwen" onclick="kama(23)">',toki:"<jan>jan: sike tawa li lon ma seme...?"},
+	{sitelen:"lonwan7",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(8)">',toki:"<jan>jan: oke, mi jo e sike tawa. <br><text>supa jan: a, sina pona!"},
+	{sitelen:"lonwan8",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(9)">',toki:"supa jan: ni la, mi ken tawa! <br>supa jan: taso... sike tawa ni li... <br>supa jan: ...jo ala e wawa. <br><br>supa jan: mi wile e sike tawa ante."},
+	{sitelen:"lonwan9",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(10)">',toki:"<jan>jan: sina wile sike tawa ante la, sina ken alasa e ona kepeken sike tawa sina. <br><text>supa jan: mi sona, mi sona."},
+	{sitelen:"lonwan10",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(11)">',toki:"<jan>jan: mi jo e supa pi ken lawa pi ken toki pi ken tawa. <br>jan: ni li lon mi. <br><text>supa jan: o toki ala."}, //10
+	{sitelen:"lonwan11",nasin:'<area shape="rect" coords="198,181,305,252" alt="sitelen toki" onclick="kama(25)"><area shape="rect" coords="356,138,525,288" alt="lupa lukin" onclick="kama(24)"><area shape="rect" coords="561,150,734,533" alt="ilo moku lete" onclick="kama(26)">',toki:"supa jan: seme li lon tomo ni...? <br>supa jan: seme li ken pana e tawa wawa tawa mi?"},
+	{sitelen:"lonwan12",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(13)">',toki:"supa jan: pona! ni la, mi ken tawa kepeken wawa! <br>supa jan: o tawa lon monsi mi. mi sona e nasin pona. <br><jan>jan: oke..."},
+	{sitelen:"lonwan13",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(14)">',toki:"<jan>jan: sina wile kepeken tomo telo, anu seme? <br><text>supa jan: ala, ala, mi lukin taso..."},
+	{sitelen:"lonwan14",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(15)">',toki:"supa jan: seme? <br><text>supa jan: mi kepeken e tomo telo tan seme? <br><br><jan>jan: mi sona ala... sina ken kepeken e ni tan... <br><br><text>supa jan: sina nasa lili. <br><jan>jan: mi sona."}, //ken la jan ale li nasa lili
+	{sitelen:"lonwan15",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(16)">',toki:"<jan>jan: a, sina wile tawa lon ma lon! <br><text>supa jan: lon! o open e lupa. <br><br><jan>jan: ... <br><jan>jan: mi ken ala..."}, //15
+	{sitelen:"lonwan16",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(17)">',toki:"<text>supa jan: tan seme? <br><jan>jan: mi jo ala e luka... <br><text>supa jan: ..."},
+	{sitelen:"lonwan17",nasin:'<area shape="rect" coords="370,212,411,409" alt="len" onclick="kama(27)"><area shape="rect" coords="453,252,505,360" alt="ilo suno" onclick="kama(28)"><area shape="rect" coords="519,140,706,457" alt="ilo suno" onclick="kama(29)">',toki:"<text>supa jan: a... ni li ike. <br>supa jan: ken la mi ken alasa e luka."},
+	{sitelen:"lonwan18",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(19)">',toki:""},
+	{sitelen:"lonwan19",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(20)">',toki:""},
+	{sitelen:"lonwan20",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(0)">',toki:"LIPU NANPA WAN LI PINI"}, //20
+	{sitelen:"alasapisiketawa1",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(6)">',toki:"<jan>jan: tenpo suno ni li pona. ken la mi tawa. <br>jan: mi ken lukin e soweli e jan e kasi e kon pi telo sewi... <br><br>jan: ... <br>jan: e"},
+	{sitelen:"alasapisiketawa2",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(6)">',toki:"<jan>jan: ni li sike tawa... taso, mi ken ala luka e ni. <br>jan: ona li lon weka... <br><br>jan: kin la, mi jo ala e luka."},
+	{sitelen:"alasapisiketawa3",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(7)">',toki:"<jan>jan: ken la, supa jan li ken kepeken e ni... <br>jan: mi ken jo e ni!"},
+	{sitelen:"alasapisiketawa9",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(11)">',toki:"supa jan: mi wile tawa ma lon! <jan><br>jan: a, sina wile ala. <br>jan: ijo ala li lon ma lon. ona li ma pi pilin ala. <text><br><br>supa jan: ... <br>supa jan: oke..."},
+	{sitelen:"alasapisiketawa4",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(11)">',toki:"supa jan: sina jo e ni la, sina jan majuna. <jan><br>jan: taso... ni li pona tawa mi. :( <text><br>supa jan: ijo ale li ken pona tawa sina! o lon, kepeken nasin pi pona sina! <jan><br>jan: a, sina pona! <text><br>supa jan: ike ala li lon, jan majuna o! <jan><br><br>jan: ..."}, //25
+	{sitelen:"alasapisiketawa5",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(12)">',toki:"supa jan: a, mi lukin. sina jo e sike pi mama waso! <jan><br>jan: ...mi wile kepeken e ni tan moku nanpa wan... <jan><br><br>jan: a, sina ken kepeken e ni. <text><br>supa jan: pona!"},
+	{sitelen:"alasapisiketawa6",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(17)">',toki:"supa jan: sina jo e ni tan seme...? <jan><br>jan: mi kepeken e ni tan... <br>jan: ...ijo esun. <text><br>supa jan: ... <br>supa jan: mi wile ala sona."},
+	{sitelen:"alasapisiketawa7",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(17)">',toki:"<jan>jan: ilo suno ni li tan mama mama mi. <text><br>supa jan: mi sona, mi ken lukin. <jan><br><br>jan: sina tan ona kin! <br><text>supa jan: ni la... <jan><br>jan: ... <text><br>supa jan: mi sama mama sina? <jan><br>jan: ...ala"},
+	{sitelen:"alasapisiketawa8",nasin:'<area shape="rect" coords="196,148,259,215" alt="kama" onclick="kama(18)">',toki:"supa jan: a, lupa ni li jo e nena pi jan pi luka ala!. <jan><br>jan: pona!"}
 ]
-palima = [6, 11, 17]
-kalamama = [9, 12, 18, 19]
+tokinimi_lontu = [
+	{sitelen:"lontu0",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(1)">',toki:"supa jan: waaa!! ma ni li pona mute! ona li suli a! <br><jan>jan: lon! ma li pona mute. <br><text>supa jan: taso tenpo pini la sina toki e ike ma... <br><br><jan>jan: ...a, mi kama pilin ante."},
+	{sitelen:"lontu1",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(2)">',toki:"supa jan: aaa, mi wile lukin e ijo mute. seme li lon? <br><jan>jan: tomo ante mute li lon!"},
+	{sitelen:"lontu2",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(3)">',toki:"<jan>jan: ken la, mi tu o tawa tomo pi esun moku. <br>jan: ni li pona ala pona tawa sina? <br><text>supa jan: a, ni li pona mute! <br><br><jan>jan: pona! <br>jan: ...mi wile e sike pi mama waso, kin..."},
+	{sitelen:"lontu3",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(4)">',toki:"<janpoka>jan lon tomo poka: a, toki jan Nika o!"},
+	{sitelen:"lontu4",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(5)">',toki:"<jan>jan Nika: a, toki, jan... lon tomo poka, o!"},
+	{sitelen:"lontu5",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(6)">',toki:"<jan>jan Nika: mi sona ala e nimi ona. <br><text>supa jan: tenpo seme la ona li lon tomo poka? <br><br><jan>jan Nika: ... <br>jan Nika: tenpo sike tu wan."},
+	{sitelen:"lontu6",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(7)">',toki:"<jan>jan Nika: mi tu o tawa!"},
+	{sitelen:"lontu7",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(8)">',toki:"supa jan: kasi ni li seme? <br><jan>jan Nika: ni li kasi pona. <br><text>supa jan: ona li pona ala pona tawa uta? <br><jan>jan Nika: pona mute! <br>jan Nika: a, nnn... mi sona ala."},
+	{sitelen:"lontu8",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(9)">',toki:"<jan>jan Nika: ni li tomo moku."},
+	{sitelen:"lontu9",nasin:'<area shape="rect" coords="0,0,800,600" alt="kama" onclick="kama(10)">',toki:""},
+]
+document.getElementById("music").src = "ala.wav";
+tokinimi = tokinimi_lonwan
+sitelennimi = "lonwan"
+kalamanimi = ["ala", "ijo_sin", "ala", "kalama", "ma_lon"]
+kalamananpa = ["lonwan0", "lonwan9", "lonawan18", "lonwan19", "lontu0"]
 
-function kama() {
-	if(palilon == false) {
-		k++
-		document.getElementById("s").src = "sitelen\\" + sitelennimi[0] + "\\" + sitelennimi[0] + k + ".png";
-		document.getElementById("n").innerHTML = tokinimi[k];
-		document.getElementById("nenapali1").innerHTML = "___";
-		document.getElementById("nenapali2").innerHTML = "___";
-		document.getElementById("nenapali3").innerHTML = "___";
-		
-		for(i = 0; i < kalamama.length; i++) {
-			if(kalamamusi == false || (k == kalamama[i])) {
-				if(k == 9) {document.getElementById("music").src = "music2.wav";}
-				else if(k == 12) {document.getElementById("music").src = "music3.wav";}
-				else if(k == 18) {document.getElementById("music").src = "music1.wav";}
-				else if(k == 19) {document.getElementById("music").src = "malon.mp3";}
-				else {
-					document.getElementById("music").src = "music1.wav";
-					kalamamusi = true
-				}
-			}
-		}
-		
-		for(i = 0; i < palima.length; i++) {
-			if(k == palima[i]) paliopen()
-		}
-	}
-	else if(palilon == true) {
-		document.getElementById("s").src = "sitelen\\" + sitelennimi[0] + "\\" + sitelennimi[0] + k + ".png";
-		document.getElementById("n").innerHTML = tokinimi[k];
-	}
+function masin(ma, nasin, toki) {
+	document.getElementById("s").src = "sitelen\\" + sitelennimi + "\\" + ma + ".png";
+	document.getElementById("n").innerHTML = toki;
+	document.getElementById("a").innerHTML = nasin;
 }
 
-function paliopen() {
-	palilon = true
-	if(k == 6) {
-		document.getElementById("nenapali1").innerHTML = "o alasa lon lupa";
-		document.getElementById("nenapali2").innerHTML = "o alasa lon ilo pi kama lete";
-		document.getElementById("nenapali3").innerHTML = "o alasa lon kiwen";
-	}
-	else if(k == 11) {
-		document.getElementById("nenapali1").innerHTML = "o lukin lon lupa";
-		document.getElementById("nenapali2").innerHTML = "o lukin lon sitelen nimi";
-		document.getElementById("nenapali3").innerHTML = "o lukin lon poki lete";
-	}
-	else if(k == 17) {
-		document.getElementById("nenapali1").innerHTML = "o alasa lon len";
-		document.getElementById("nenapali2").innerHTML = "o alasa lon ilo pi suno lili";
-		document.getElementById("nenapali3").innerHTML = "o alasa lon lupa";
-	}
-}
-
-function pali1() {
-	if(palilon == true) {
-		if(k == 6) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa1.png";
-			document.getElementById("n").innerHTML = "<jan>jan: tenpo suno ni li pona. ken la mi tawa. <br>jan: mi ken lukin e soweli e jan e kasi e kon pi telo sewi... <br><br>jan: ... <br>jan: e";
-		}
-		else if(k == 11) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa1.png";
-			document.getElementById("n").innerHTML = "supa jan: mi wile tawa ma lon! <jan><br>jan: a, sina wile ala. <br>jan: ijo ala li lon ma lon. ona li ma pi pilin ala. <text><br><br>supa jan: ... <br>supa jan: oke...";
-		}
-		else if(k == 17) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa6.png";
-			document.getElementById("n").innerHTML = "supa jan: sina jo e ni tan seme...? <jan><br>jan: mi kepeken e ni tan... <br>jan: ...ijo esun. <text><br>supa jan: ... <br>supa jan: mi wile ala sona.";
+function kama(sona) {
+	masin(tokinimi[sona].sitelen, tokinimi[sona].nasin, tokinimi[sona].toki)
+	
+	for(i = 0; i < kalamananpa.length; i++) {
+		if(tokinimi[sona].sitelen == kalamananpa[i]) {
+			document.getElementById("music").src = kalamanimi[i]+".mp3";
 		}
 	}
-}
-
-function pali2() {
-	if(palilon == true) {
-		if(k == 6) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa2.png";
-			document.getElementById("n").innerHTML = "<jan>jan: ni li sike tawa... taso, mi ken ala luka e ni. <br>jan: ona li lon ma suli... <br><br>jan: kin la, mi jo ala e luka";
-		}
-		else if(k == 11) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa4.png";
-			document.getElementById("n").innerHTML = "supa jan: sina jo e ni la, sina jan Puma. <jan><br>jan: taso... ni li pona tawa mi. :( <text><br>supa jan: ijo ale li ken pona tawa sina! o lon, kepeken nasin pi pona sina! <jan><br>jan: a, sina pona! <text><br>supa jan: ike ala li lon, jan Puma o! <jan><br><br>jan: ...";
-		}
-		else if(k == 17) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa7.png";
-			document.getElementById("n").innerHTML = "<jan>jan: ilo ni pi suno lili li tan mama mama mi. <text><br>supa jan: mi sona, mi ken lukin. <jan><br><br>jan: sina tan ona kin! <br><text>supa jan: ni la... <jan><br>jan: ... <text><br>supa jan: mi pata pi mama sina? <jan><br>jan: ...ala";
-		}
-	}
-}
-
-function pali3() {
-	if(palilon == true) {
-		if(k == 6) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa3.png";
-			document.getElementById("n").innerHTML = "<jan>jan: ken la, supa jan li ken kepeken e ni... <jan>jan: ken la, supa jan li ken kepeken e ni...  <jan>jan: mi ken jo e ni. ";
-			palilon = false
-		}
-		else if(k == 11) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa5.png";
-			document.getElementById("n").innerHTML = "supa jan: a, mi sona. sina jo e sike pi mama waso! <jan><br>jan: ...mi wile kepeken e ni tan moku nanpa wan... <jan><br><br>jan: a, sina ken kepeken e ni. <text><br>supa jan: pona!";
-			palilon = false
-		}
-		else if(k == 17) {
-			document.getElementById("s").src = "sitelen\\alasapisiketawa8.png";
-			document.getElementById("n").innerHTML = "supa jan: a, lupa ni li jo e nena pi jan pi luka ala!. <jan><br>jan: pona!";
-			palilon = false
-		}
+	
+	if(sona == 20) {
+		tokinimi = tokinimi_lontu;
+		sitelennimi = "lontu"
 	}
 }
